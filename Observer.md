27d7d16eaff77fa21fb4fa39f517b1b8a5a6e0cb
David Gristwood <davidgri.ms@hotmail.com>
2014-07-22 15:49:19 +0100

    sample for observer pattern

--- Observer.md
+++ Observer.md
@@ -2,21 +2,43 @@
 
 ## Intent
 
+Allows an observer to be notified of any state changes that a grain can choose to publish
+
 ## Also Known As
+
+Pub/Sub, Events
 
 ## Motivation
 
+The observer pattern is a well known software design pattern in which an object, called the subject, maintains a list of interested parties, called observers, and notifies them automatically of any state changes, usually by calling one of their methods. Because this pattern is so well established, Orleans supports it natively, along with helper classes to aid implementation. 
+
 ## Applicability
+
+You would use the Observer pattern when want a grain to allow subscribers to register their interest in specific events.   
 
 ## Structure
 
 ## Participants
+
+The subject (aka observed) grain
+
+The observer grain
+
 
 ## Collaborations
 
 ## Consequences
 
 ## Implementation
+
+A grain type that supports observation will define an observer interface that inherits from the IGrainObserver interface. Methods on this observer interface correspond to events that the observed grain makes available. 
+
+An observer implements this interface and then subscribe to notifications from a particular grain. The observed grain would call back to the observer through the observer interface methods when an event has occurred.
+
+Methods on observer interfaces must be void since event messages are one-way. If the observer needs to interact with the observed grain as a result of a notification, it must do so by invoking normal methods on the observed grain.
+
+The observed grain type must expose a method to allow observers to subscribe to event notifications from a grain. In addition, it is usually convenient to expose a method that allows an existing subscription to be canceled. Grain developers may use the Orleans ObserverSubscriptionManager<T> class to simplify the subscriptions and notifications.
+
 
 ## Sample Code
 
---
+++ samples/Observer/Observer/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ObserverPattern
+{
+    using ObserverInterfaces;
+
+    /// <summary>
+    /// Orleans test silo host
+    /// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // The Orleans silo environment is initialized in its own app domain in order to more
+            // closely emulate the distributed situation, when the client and the server cannot
+            // pass data via shared memory.
+            AppDomain hostDomain = AppDomain.CreateDomain("OrleansHost", null, new AppDomainSetup
+            {
+                AppDomainInitializer = InitSilo,
+                AppDomainInitializerArguments = args,
+            });
+
+            Orleans.OrleansClient.Initialize("DevTestClientConfiguration.xml");
+
+            // Observer
+
+            IDoStuff grain = DoStuffFactory.GetGrain(0);
+
+            var theObserver = new TheObserver();
+
+            var obj = ObserveFactory.CreateObjectReference(theObserver).Result; // factory form IObserve
+
+            grain.SubscribeForUpdates(obj);
+            
+
+            // close down
+
+            Console.WriteLine("Orleans Silo is running.\nPress Enter to terminate...");
+            Console.ReadLine();
+
+            hostDomain.DoCallBack(ShutdownSilo);
+        }
+
+
+        // class for handling updates from grain
+        private class TheObserver : IObserve
+        {
+            // Receive updates 
+            public void StuffUpdate(int data)
+            {
+                Console.WriteLine("New stuff: {0}", data);
+            }
+        } 
+
+
+
+        static void InitSilo(string[] args)
+        {
+            hostWrapper = new OrleansHostWrapper(args);
+
+            if (!hostWrapper.Run())
+            {
+                Console.Error.WriteLine("Failed to initialize Orleans silo");
+            }
+        }
+
+        static void ShutdownSilo()
+        {
+            if (hostWrapper != null)
+            {
+                hostWrapper.Dispose();
+                GC.SuppressFinalize(hostWrapper);
+            }
+        }
+
+        private static OrleansHostWrapper hostWrapper;
+    }
+}
