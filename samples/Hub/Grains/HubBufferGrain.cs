90d175472aa699a0ccce03ec24c7c7076499df3e
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2014-12-14 19:03:22 +0200

    Buffer grain is a stateless worker so there is no need to pin it via delaydeactivation

--- samples/Hub/Grains/HubBufferGrain.cs
+++ samples/Hub/Grains/HubBufferGrain.cs
@@ -12,30 +12,16 @@ namespace Sample
     public class HubBufferGrain : Grain, IHubBuffer
     {
         readonly TimeSpan flushPeriod = TimeSpan.FromSeconds(1);
-        readonly TimeSpan keepAlivePeriod = TimeSpan.FromHours(12);
 
         IHub hub;
         Queue<Notification> buffer;
-
-        public Task Init()
-        {
-            return TaskDone.Done;
-        }
 
         public override Task ActivateAsync()
         {
             hub = HubGateway.GetLocalHub();
             buffer = new Queue<Notification>();
-
-            RegisterTimer(KeepAlive, null, keepAlivePeriod, keepAlivePeriod);
+            
             RegisterTimer(Flush, null, flushPeriod, flushPeriod);
-
-            return TaskDone.Done;
-        }
-
-        Task KeepAlive(object arg)
-        {
-            DelayDeactivation(keepAlivePeriod);
             return TaskDone.Done;
         }
 
